e578b61665f651be2b4fa8b2e78f67ee9c61ebba
EllizzInBits <elizabethm174394@gmail.com>
2025-09-04 09:23:33 -0300

    config

---
+++ mvnw.cmd
@@ -0,0 +1,5 @@
+@REM Maven Wrapper script for Windows
+@echo off
+set MAVEN_PROJECTBASEDIR=%~dp0
+set MAVEN_OPTS=%MAVEN_OPTS% -Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%
+java -jar "%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar" %*
